87505cc8cab9a3bcbec8879d8841353cdd1de066
pisslake <pisslake@users.noreply.github.com>
2020-11-13 15:43:20 -0800

    finished tutorial

--- quickstart-react/src/App.js
+++ quickstart-react/src/App.js
@@ -19,6 +19,18 @@ class App extends React.Component {
     monday.listen("settings", res => {
       this.setState({ settings: res.data });
     });
+
+    monday.listen("context", res => {
+      this.setState({context: res.data});
+      console.log(res.data);
+      monday.api(`query ($boardIds: [Int]) { boards (ids:$boardIds) { name items(limit:1) { name column_values { title text } } } }`,
+        { variables: {boardIds: this.state.context.boardIds} }
+      )
+      .then(res => {
+        this.setState({boardData: res.data});
+      });
+    })
+
   }
 
   render() {
@@ -27,11 +39,15 @@ class App extends React.Component {
         className="App"
         style={{background: (this.state.settings.background)}}
       >
+      {JSON.stringify(this.state.boardData, null, 2)} 
       <h3>
         What's up bitches? You're gonna choke on this node graph.
       </h3>
 
       <div> please help me </div>
+      <p>
+        dear lord above, i pray
+      </p>
 
       </div>
     );
