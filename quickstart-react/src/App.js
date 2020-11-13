7c0b51c9df3ca58716dbee872390de5183070800
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2020-11-13 15:08:48 -0800

    Added react

---
+++ quickstart-react/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+import mondaySdk from "monday-sdk-js";
+const monday = mondaySdk();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Default state
+    this.state = {
+      settings: {},
+      name: "",
+    };
+  }
+
+  // use to communicate with event listeners
+  componentDidMount() {
+    monday.listen("settings", res => {
+      this.setState({ settings: res.data });
+    });
+  }
+
+  render() {
+    return (
+      <div 
+        className="App"
+        style={{background: (this.state.settings.background)}}
+      >
+      <h3>
+        What's up bitches? You're gonna choke on this node graph.
+      </h3>
+      </div>
+    );
+  }
+}
+
+export default App;
