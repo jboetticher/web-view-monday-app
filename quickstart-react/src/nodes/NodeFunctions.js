b9e3aae645ca369628707d2dbf92cef803f926cc
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2020-11-20 00:37:46 -0800

    node saving functions finished but not tested

--- quickstart-react/src/nodes/NodeFunctions.js
+++ quickstart-react/src/nodes/NodeFunctions.js
@@ -6,16 +6,60 @@ class NodeFunctions {
 
     constructor(mondaySdk) {
         this.monday = mondaySdk;
+        this.QueryConnections();
     }
 
-    /* Adds and saves connections between two nodes.
-    * onConnectParams - the parameters that you get 
+    /* Queries the connection data from monday.com.
+     */
+    QueryConnections() {
+        this.monday.storage.instance.getItem('connection_objects').then(res => {
+            if (res.data.value !== null) {
+                this.connections = JSON.parse(res.data.value);
+            }
+            else {
+                this.connections = [];
+            }
+        });
+    }
+
+    /* Returns the connection data from monday.com in a object format.
+     */
+    GetConnections() {
+        return this.connections;
+    }
+
+    /* 
+    * Adds and saves connections between two nodes.
+    * onConnectParams - the parameters that you get when a connection is made
     */
     AddConnection(onConnectParams) {
-        this.monday.storage.instance.setItem('mykey', 'Lorem Ipsum').then(res => {
 
-            
+        // if it's null we ain't messing with it
+        if (onConnectParams == null) { return; }
 
+        // checks to see if the current array has any of those connections
+        var replaceIndex = -1;
+        for (var i = 0; i < this.connections.length; i++) {
+            if (this.connections[i].source == onConnectParams.source &&
+                this.connections[i].target == onConnectParams.target) {
+                replaceIndex = i;
+                break;
+            }
+        }
+
+        // add or replace to the current array of connections 
+        if (replaceIndex < 0) {
+            this.connections.push(onConnectParams);
+        }
+        else {
+            this.connections[i] = onConnectParams;
+        }
+
+        // json stringify the current connections
+        const jsonString = JSON.stringify(this.connections);
+
+        // save to monday.com persist
+        this.monday.storage.instance.setItem('connection_objects', jsonString).then(res => {
             console.log(res);
         });
     }
