e40f587d309a832a8a1c71730b3996c3f46674a0
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2020-11-20 21:14:57 -0800

    Created shoddy collapse and no collapse

--- quickstart-react/src/nodes/PrettyItemNode.js
+++ quickstart-react/src/nodes/PrettyItemNode.js
@@ -1,8 +1,9 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import { Handle } from 'react-flow-renderer';
 import ChevronDown from "monday-ui-react-core/dist/icons/DropdownChevronDown";
 import ChevronUp from "monday-ui-react-core/dist/icons/DropdownChevronUp";
 import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
 
 // todo: make color of node change based on whether or not it's finished
 // todo: make the color of the dependent nodes change based on whether or not it's finished
@@ -10,20 +11,41 @@ import Chip from '@material-ui/core/Chip';
 
 export default memo(({ data }) => {
 
+    // collapsed vs not collapsed
+    let [collapsed, setCollapsed] = useState(true);
+    var chevronDownStyle = collapsed ? { bottom: '-2px' } : { top: '-2px' };
+    var chevronUpStyle = collapsed ? { top: '-2px' } : { bottom: '-2px' };
+    var collapseButtonChip =
+        <Avatar className={"small-avatar pos-absolute collapse-chip"}
+            style={{ left: "-10px", top: "-10px", background: "#666" }}
+            onClick={() => {
+                console.log('u just clicked it');
+                setCollapsed(!collapsed);
+            }}
+        >
+            <ChevronUp className={"pos-absolute"} style={chevronUpStyle} />
+            <ChevronDown className={"pos-absolute"} style={chevronDownStyle} />
+        </Avatar>;
+    var notCollapsedData = collapsed ? <div/> :
+        <div>
+            Group: {data?.group}
+        </div>;
+
+
     // change what the handles look like if it is in connecting mode
     // doesnt work so don't give up 
-    var targetStyle = { background: '#0071d9', width: '10px', height:'10px'};    
-    var sourceStyle = { background: 'var(--color-mud_black)', width: '10px', height:'10px' };
-    if(data?.isConnecting){
-        targetStyle = { background: '#0071d9', width: '20px', height:'20px' };
+    var targetStyle = { background: '#0071d9', width: '10px', height: '10px' };
+    var sourceStyle = { background: 'var(--color-mud_black)', width: '10px', height: '10px' };
+    if (data?.isConnecting) {
+        targetStyle = { background: '#0071d9', width: '20px', height: '20px' };
         //console.log("handle style changed");
     }
     else {
-        targetStyle = { background: '#0071d9', width: '10px', height:'10px' };
+        targetStyle = { background: '#0071d9', width: '10px', height: '10px' };
         //console.log("default handle style");
     }
 
-    
+
     return (
         <>
             <div style={{
@@ -39,16 +61,15 @@ export default memo(({ data }) => {
                     height: "8px",
                     bottom: "0px", left: "0px", right: "0px"
                 }} />
+                {collapseButtonChip}
             </div>
-
-                <Chip></Chip>
 
             <Handle
                 type="target"
                 position="top"
                 id="t"
                 style={targetStyle}
-            />  
+            />
             <Handle
                 type="source"
                 position="bottom"
@@ -68,16 +89,16 @@ export default memo(({ data }) => {
                 style={sourceStyle}
             />
 
-            <h4>{data?.label}</h4>
-            <div style={{ maxWidth: "150px" }}>
-                <h4>Group: {data?.group}</h4>
-                <p>{data?.title}</p>
+            <div style={{ maxWidth: "170px" }}>
+                <h4>{data?.title}</h4>
+                {notCollapsedData}
             </div>
-            <div style={{   
+            <div style={{
                 textAlign: "center",
                 background: data?.statusData['color'],
                 borderRadius: "4px",
-                padding: "4px"
+                padding: "4px",
+                marginTop: "8px"
             }}>
                 {data?.statusData['status'] === "" ? "Empty" : data.statusData['status']}
             </div>
