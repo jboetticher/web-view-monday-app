a416ab10ba92c4f38a01ec03a810c63493939984
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2020-11-25 23:03:24 -0800

    Find priority button yay

--- quickstart-react/src/components/FindPriorityButton.js
+++ quickstart-react/src/components/FindPriorityButton.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Button from "monday-ui-react-core/dist/Button.js";
 import "monday-ui-react-core/dist/Button.css";
-import ReactFlow, { useZoomPanHelper } from 'react-flow-renderer';
+import ReactFlow, { useZoomPanHelper, useStoreState, useStoreActions } from 'react-flow-renderer';
 
 let FindPriorityButton = props => {
 
-    const { zoomOut } = useZoomPanHelper();
+    const { transform } = useZoomPanHelper();
+    const [nodes] = useStoreState((store) => store.nodes);
+
+    function FindPriority() {
+
+    }
 
     return (
         <Button
             size="small"
             style={{ marginRight: "8px" }}
             onClick={(e) => {
-                zoomOut();
+                transform({x: 100, y: 200, zoom: 1});
+                console.log(nodes);
             }}>
             Find Priority
         </Button>
